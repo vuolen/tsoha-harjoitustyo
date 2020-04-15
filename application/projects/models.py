b9ed9e8b70754050d8db0f30fbd6dd3ce3ea6632
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2020-04-15 12:52:14 +0300

    add one to many relationship between a project and stages

--- application/projects/models.py
+++ application/projects/models.py
@@ -4,5 +4,7 @@ from application.models import Base
 class Project(Base):
     name = db.Column(db.String(144), nullable=False)
 
+    stages = db.relationship("Stage", backref="project", lazy=True)
+    
     def __init__(self, name):
         self.name = name;
